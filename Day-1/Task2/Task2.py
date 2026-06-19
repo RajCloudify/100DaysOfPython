ccb5e755eccb1b29a61690a3a7da03ac1af5ebe3
RajCloudify <rajattingal1@gmail.com>
2026-06-19 16:56:01 +0530

    String Manipulation n to shift to the next line

---
+++ Day-1/Task2/Task2.py
@@ -0,0 +1,6 @@
+# Write your code below this line 👇
+print("Hello there!\nThis is my First Code\nAnd it is going well")
+# "\n" ---> Here is used to Shift to the next line and print the content 
+
+
+
