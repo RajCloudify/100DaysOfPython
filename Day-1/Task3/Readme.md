70e3d3f16a9a73999c7f4893c219599ff7803ddf
RajCloudify <rajattingal1@gmail.com>
2026-06-19 17:01:11 +0530

    Adding Input function in order to collect user input and to use in the code

---
+++ Day-1/Task3/Readme.md
@@ -0,0 +1,10 @@
+Using the Python input() function to collect user input and use it within your code.
+
+
+Looking to print a sentence like this:
+
+Hello Name!
+
+e.g. Hello Raj!
+
+
---
+++ Day-1/Task3/Task3.py
@@ -0,0 +1,4 @@
+print("Hello " + input(" What is your name?") + "!")
+#Use the input function to ask a certain input and printed it here along with the pre-defined strings and added the "!" string also 
+
+
