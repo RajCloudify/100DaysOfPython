fa947a22cf56e6f4786e03de9c6740eb876af57d
RajCloudify <rajattingal1@gmail.com>
2026-06-22 22:04:21 +0530

    Task 4 variables

---
+++ Day-1/Task4/Task4.py
@@ -0,0 +1,8 @@
+print (len(input("What is your name?")))
+# Q1. The above code is aimed to be in one line by defining the length of the input strings
+
+
+# Q2. Gave input and called inside a variable and printed the length of the string 
+
+username = input("What is your favourite sport?")
+print (len(username))
